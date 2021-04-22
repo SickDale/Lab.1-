e4ae5d10df9486d3de51363d8c553eb3effe6270
Daniel72674 <56113994+Daniel72674@users.noreply.github.com>
2021-04-23 00:08:22 +0200

    Cwiczenie 10e

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,33 +1,19 @@
 package pl.lublin.wsei.java.cwiczenia; // deklaracja pakietu package lab1
 
+import pl.lublin.wsei.java.cwiczenia.mylib.Account;
+
 import java.util.Scanner;
-import java.util.Locale;
 
 
 
-public class /*definicja klasy public class Main*/ Main {
+//12
+public class Main{
 
-    private static String leftPad(String aText, char aChar, int aWidth)
-    {
-        String res = aText;
-        for (int i = 0; i < aWidth * aText.length(); i++)
-            res = aChar + res;
-        return res;
-    }
-
-
-    public static void main(String[] args) /*
-    *definicja metody public static void main*/{
-
-//10c
-        Scanner input = new Scanner(System.in);
-        int num1;
-        System.out.print("Podaj liczbę do przekonwertowania na typ szesnastkowy ");
-        num1 = input.nextInt();
-
-        String hexNum = leftPad(Integer.toHexString(num1), '0', 3);
-        String binNum = leftPad(Integer.toBinaryString(num1), '0', 1);
-        System.out.println("liczba " + num1 + " w:\nPostaci szesnastkowej to " + hexNum.toUpperCase() + "\nPostaci binarnej to " + binNum.toUpperCase() );
+    public static void main(String[] args) {
+        Account acc = new Account();
+        acc.name = "Piotr Gołabek";
+        System.out.println(acc.getName());
 
     }
+
 }
---
+++ src/pl/lublin/wsei/java/cwiczenia/mylib/Account.java
@@ -0,0 +1,19 @@
+package pl.lublin.wsei.java.cwiczenia.mylib;
+//12
+
+public class Account {
+    public String name;
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+// public void setName(String name) {
+// this.name = name;
+// }
+//
+// public String getName() {
+// return name;
+// }
+}
