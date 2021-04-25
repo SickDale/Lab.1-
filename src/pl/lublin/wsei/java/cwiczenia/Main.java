bcd0bf3282b1e66ccb7f1651955c8e7e05ebe93c
Daniel72674 <56113994+Daniel72674@users.noreply.github.com>
2021-04-26 00:18:29 +0200

    poprawione 12-translacja

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -2,19 +2,17 @@ package pl.lublin.wsei.java.cwiczenia; // deklaracja pakietu package lab1
 
 import pl.lublin.wsei.java.cwiczenia.mylib.Account;
 
-import java.util.Scanner;
-
 
 
 //12
-public class Main{
+public class Main {
 
     public static void main(String[] args) {
+
         Account acc = new Account();
-        acc.setName ("piotr gołabek");
-         System.out.println(acc.getName());
-        System.out.printf("%s%n",Account.capitalize("StanisŁaw maruSARz"));
+        acc.setName("абвгдеєжзиіїйклмнопрстуфхцчшщьюя");
+        System.out.println(acc.getName());
+
 
     }
-
 }
--- src/pl/lublin/wsei/java/cwiczenia/mylib/Account.java
+++ src/pl/lublin/wsei/java/cwiczenia/mylib/Account.java
@@ -1,26 +1,39 @@
 package pl.lublin.wsei.java.cwiczenia.mylib;
-import org.apache.commons.lang3.StringUtils;
 
 //12
 
 public class Account {
-    private String name;
-
- public static String capitalize(String aStr)
- {
-  String[] tokens = StringUtils.split(aStr);
-  for(int i = 0; i < tokens.length; i++)
-   tokens[i] = StringUtils.capitalize(tokens[i]);
-  return StringUtils.join(tokens,' ');
- }
- public void setName(String name) {
-  this.name = capitalize(name);
- }
+ private String name;
 
  public String getName() {
- return name;
+  return name;
  }
 
+ public static String translit(String arg) {
+  String ukrAlphabet = "абвгдеєжзиіїйклмнопрстуфхцчшщьюя";
+  char[] ukrChar = ukrAlphabet.toCharArray();
+  System.out.println(ukrAlphabet);
+  String[] translitRepl = {"a", "b", "v", "g", "d", "e", "je", "zh", "z", "y", "i", "ji", "j",
+          "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "f", "h", "c",
+          "ch", "sh", "shh", "'", "ju", "ja"};
+  char[] tokens = arg.toCharArray();
+
+
+  String output = "";
+  for (int i = 0; i < tokens.length; i++)
+
+   for (int j = 0; j < ukrChar.length; j++) {
+    if (tokens[i] == ukrChar[j]) {
+     output = output + translitRepl[j];
+     break;
+    }
+   }
+
+  return output;
+ }
+
+ public void setName(String name) {
+  this.name = translit(name);
+ }
 
 }
-
